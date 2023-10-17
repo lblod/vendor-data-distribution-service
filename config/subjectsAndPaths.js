a13b16c5b7148e5c7917cb13a996f70764ba683a
benjay10 <ben.behaeghe@redpencil.io>
2023-10-17 15:19:04 +0200

    Make example config for new VDDS mechanisms

--- config/subjectsAndPaths.js
+++ config/subjectsAndPaths.js
@@ -1,10 +1,29 @@
 export const subjects = [
   {
     type: 'http://rdf.myexperiment.org/ontologies/base/Submission',
+    trigger: `
+      ?subject a <http://rdf.myexperiment.org/ontologies/base/Submission> .
+    `,
     path: `
       ?subject
         pav:createdBy ?organisation ;
         pav:providedBy ?vendor .
     `,
+    remove: {
+      delete: `
+        ?subject ?p ?o .
+      `,
+      where: `
+        ?subject ?p ?o .
+      `,
+    },
+    copy: {
+      insert: `
+        ?subject ?p ?o .
+      `,
+      where: `
+        ?subject ?p ?o .
+      `,
+    },
   },
 ];
